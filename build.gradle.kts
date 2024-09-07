56dbfb31c795d0e720fc6f32389679d3d3b708a7
vickyleu <yu6564172@gmail.com>
2024-09-08 04:06:28 +0800

    reformat to kmp in progress

---
+++ build.gradle.kts
@@ -0,0 +1,9 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    alias(libs.plugins.kotlinMultiplatform)  apply false
+//    id 'com.android.application' version '8.1.2' apply false
+//    id 'com.android.library' version '8.1.2' apply false
+//    id 'org.jetbrains.kotlin.android' version '1.9.0' apply false
+//    id 'org.jetbrains.dokka' version '1.9.20'
+//    id 'com.vanniktech.maven.publish' version '0.25.3' apply false
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,52 @@
+rootProject.name = "UltraSwipeRefresh"
+
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+
+pluginManagement {
+    repositories.apply {
+        removeAll(this)
+    }
+    dependencyResolutionManagement.repositories.apply {
+        removeAll(this)
+    }
+    listOf(repositories, dependencyResolutionManagement.repositories).forEach {
+        it.apply {
+            gradlePluginPortal()
+            mavenCentral()
+            maven(url = "https://openbakery.org/repository/")
+        }
+    }
+}
+
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.7.0"
+}
+
+
+dependencyResolutionManagement {
+    //FAIL_ON_PROJECT_REPOS
+//    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
+
+    repositories {
+        mavenCentral()
+        google()
+        // workaround for https://youtrack.jetbrains.com/issue/KT-51379
+        maven {
+            setUrl("https://repo.maven.apache.org/maven2")
+            content {
+                excludeGroupByRegex("com.finogeeks.*")
+                excludeGroupByRegex("org.jogamp.*")
+                excludeGroupByRegex("com.vickyleu.*")
+                excludeGroupByRegex("com.github.(?!johnrengelman|oshi|bumptech|mzule|pwittchen|filippudak|asyl|florent37).*")
+            }
+        }
+    }
+}
+
+
+//include(":app")
+//include(":refresh")
+//include(":refresh-indicator-classic")
+//include(":refresh-indicator-progress")
+//include(":refresh-indicator-lottie")
