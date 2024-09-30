8a61a6cc673860ed02c059d73d0eecf37f01a648
vicky leu <yu6564172@gmail.com>
2024-09-30 12:10:01 +0800

    refactored into compose multiplatform, downgrade kotlin version

--- refresh-indicator-classic/src/iosMain/kotlin/com/king/ultraswiperefresh/indicator/classic/SimpleDateFormat.ios.kt
+++ refresh-indicator-classic/src/iosMain/kotlin/com/king/ultraswiperefresh/indicator/classic/SimpleDateFormat.ios.kt
@@ -1,10 +1,21 @@
 package com.king.ultraswiperefresh.indicator.classic
 
 import androidx.compose.ui.text.intl.Locale
+import platform.Foundation.NSDate
+import platform.Foundation.NSDateFormatter
+import platform.Foundation.NSLocale
 
-actual class SimpleDateFormatImpl actual constructor(val pattern: String,val locale: Locale) {
+actual class SimpleDateFormatImpl actual constructor(
+    private val pattern: String,
+    private val locale: Locale
+) {
+    private val dateFormatter: NSDateFormatter = NSDateFormatter().apply {
+        setDateFormat(this@SimpleDateFormatImpl.pattern)
+        setLocale(NSLocale(this@SimpleDateFormatImpl.locale.language))
+    }
 
     actual fun format(time: Long): String {
-        TODO("Not yet implemented")
+        val date = NSDate(time / 1000.0)  // 将毫秒转换为秒
+        return dateFormatter.stringFromDate(date)
     }
 }
